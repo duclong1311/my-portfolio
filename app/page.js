7ce66be7f4ac7f158076e31795c92b976ee0f168
duclong1311 <duclong1311.work@gmail.com>
2024-12-06 13:11:36 +0700

    delete blog and unuse relation

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
@@ -7,29 +6,8 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-import dynamic from "next/dynamic";
-
-const personalDataDynamic = dynamic(() => import("@/utils/data/personal-data"), {
-  ssr: false,
-});
-
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalDataDynamic.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
 
 export default async function Home() {
-  const blogs = await getData();
-
   return (
     <>
       <HeroSection />
@@ -38,7 +16,7 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
-      <Blog blogs={blogs} />
+      <Blog/>
       <ContactSection />
     </>
   )
